434de89cffd0cae74699fdabf4a77edfb24e261a
innomon <tech@innomon.in>
2016-06-10 14:08:14 +0530

    Initial commit

---
+++ src/in/innomon/je/EnvInfo.java
@@ -0,0 +1,106 @@
+/*
+* Copyright (c) 2016, BON BIZ IT Services Pvt LTD.
+*
+* The Universal Permissive License (UPL), Version 1.0
+* 
+* Subject to the condition set forth below, permission is hereby granted to any person obtaining a copy of this software, associated documentation and/or data (collectively the "Software"), free of charge and under any and all copyright rights in the Software, and any and all patent rights owned or freely licensable by each licensor hereunder covering either (i) the unmodified Software as contributed to or provided by such licensor, or (ii) the Larger Works (as defined below), to deal in both
+
+* (a) the Software, and
+
+* (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if one is included with the Software (each a “Larger Work” to which the Software is contributed by such licensors),
+* 
+* without restriction, including without limitation the rights to copy, create derivative works of, display, perform, and distribute the Software and make, use, sell, offer for sale, import, export, have made, and have sold the Software and the Larger Work(s), and to sublicense the foregoing rights on either these or other terms.
+* 
+* This license is subject to the following condition:
+* 
+* The above copyright notice and either this complete permission notice or at a minimum a reference to the UPL must be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* Author: Ashish Banerjee, tech@innomon.in
+*/
+
+package in.innomon.je;
+
+/**
+ *
+ * @author ashish
+ * 22-May-11: Added BDB JE Replication extensions 
+ */
+public class EnvInfo {
+    private String envHome = "/home/ashish/ubpay";
+    private String balanceStore = "CashBalance";
+    private String txnStore = "TxnStore";
+    private boolean createAccountOnTxn = false;  // create a new UUID if not present
+
+    // replication support
+    private String groupName = "GpayReplicationGroup";
+    private String nodeName  = "Master";
+    private String nodeHostPort = "localhost:4101";
+    private String helperHosts = "localhost:4101";
+    
+    public String getTxnStore() {
+        return txnStore;
+    }
+
+    public void setTxnStore(String txnStore) {
+        this.txnStore = txnStore;
+    }
+
+    public String getBalanceStore() {
+        return balanceStore;
+    }
+
+    public void setBalanceStore(String balanceStore) {
+        this.balanceStore = balanceStore;
+    }
+
+    public boolean isCreateAccountOnTxn() {
+        return createAccountOnTxn;
+    }
+
+    public void setCreateAccountOnTxn(boolean createAccountOnTxn) {
+        this.createAccountOnTxn = createAccountOnTxn;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getHelperHosts() {
+        return helperHosts;
+    }
+
+    public void setHelperHosts(String helperHosts) {
+        this.helperHosts = helperHosts;
+    }
+
+    public String getNodeHostPort() {
+        return nodeHostPort;
+    }
+
+    public void setNodeHostPort(String nodeHostPort) {
+        this.nodeHostPort = nodeHostPort;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getEnvHome() {
+        return envHome;
+    }
+
+    public void setEnvHome(String envHome) {
+        this.envHome = envHome;
+    }
+
+}
---
+++ src/in/innomon/je/JeDbLifeCycle.java
@@ -0,0 +1,75 @@
+/*
+* Copyright (c) 2016, BON BIZ IT Services Pvt LTD.
+*
+* The Universal Permissive License (UPL), Version 1.0
+* 
+* Subject to the condition set forth below, permission is hereby granted to any person obtaining a copy of this software, associated documentation and/or data (collectively the "Software"), free of charge and under any and all copyright rights in the Software, and any and all patent rights owned or freely licensable by each licensor hereunder covering either (i) the unmodified Software as contributed to or provided by such licensor, or (ii) the Larger Works (as defined below), to deal in both
+
+* (a) the Software, and
+
+* (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if one is included with the Software (each a “Larger Work” to which the Software is contributed by such licensors),
+* 
+* without restriction, including without limitation the rights to copy, create derivative works of, display, perform, and distribute the Software and make, use, sell, offer for sale, import, export, have made, and have sold the Software and the Larger Work(s), and to sublicense the foregoing rights on either these or other terms.
+* 
+* This license is subject to the following condition:
+* 
+* The above copyright notice and either this complete permission notice or at a minimum a reference to the UPL must be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* Author: Ashish Banerjee, tech@innomon.in
+*/
+
+package in.innomon.je;
+
+import com.sleepycat.je.Environment;
+import com.sleepycat.je.EnvironmentConfig;
+import in.innomon.util.LifeCycle;
+import java.io.File;
+
+/**
+ *
+ * @author ashish
+ */
+public abstract class JeDbLifeCycle implements LifeCycle {
+    protected Environment env = null;
+
+    public JeDbLifeCycle() {
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        stop();
+        super.finalize();
+       
+    }
+    protected EnvInfo envInfo = new EnvInfo();
+
+    public void setEnvInfo(EnvInfo env) {
+        this.envInfo = env;
+    }
+
+    @Override
+    public void start() {
+        if (env == null) {
+            // Set up the environment.
+            EnvironmentConfig envConfig = new EnvironmentConfig();
+            envConfig.setAllowCreate(true);
+            envConfig.setTransactional(true);
+            //  Environment handles are free-threaded by default in JE,
+            // so we do not have to do anything to cause the
+            // environment handle to be free-threaded.
+            // Open the environment
+            env = new Environment(new File(envInfo.getEnvHome()), envConfig);
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (env != null) {
+            env.close();
+            env = null;
+        }
+    }
+    
+}
